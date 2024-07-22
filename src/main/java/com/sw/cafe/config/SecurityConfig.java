04c1d54e1ef3495ab72f023af9e8bba55ae79271
Ugo Gemesio <86371597+ugogemesio@users.noreply.github.com>
2024-07-22 11:28:51 -0300

    Update SecurityConfig.java

--- src/main/java/com/sw/cafe/config/SecurityConfig.java
+++ src/main/java/com/sw/cafe/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/login", "/register", "/colaborador", "/css/**").permitAll()
+                .requestMatchers("/register", "/login", "/colaborador", "/css/**").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
