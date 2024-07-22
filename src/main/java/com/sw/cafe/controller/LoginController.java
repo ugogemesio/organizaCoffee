0da9efd8292740a5e3d3bf21de96867f2c374174
Ugo Gemesio <86371597+ugogemesio@users.noreply.github.com>
2024-07-22 11:47:29 -0300

    Update LoginController.java

--- src/main/java/com/sw/cafe/controller/LoginController.java
+++ src/main/java/com/sw/cafe/controller/LoginController.java
@@ -6,17 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    // @GetMapping("/login")
-    // public String login() {
-    //     return "login";
-    // }
-
     @GetMapping("/login")
     public String home() {
-        return "login";
+        return "loginForm";
     }
 
-    @GetMapping("/")
+    @GetMapping("/register")
     public String registra() {
         return "registraForm";
     }
